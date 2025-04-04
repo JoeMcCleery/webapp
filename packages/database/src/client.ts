b47bd09705ba5a885d9685bf61e670a7adb0644b
JoeMcCleery <mccleery.joseph@gmail.com>
2025-04-05 00:29:10 +0800

    Update database package

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -3,10 +3,6 @@ import { enhance } from "@zenstackhq/runtime";
 
 export const prisma = new PrismaClient();
 
-const globalForPrisma = global as unknown as { prisma: typeof prisma };
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
 export function getEnhancedPrisma(user?: any) {
   return enhance(prisma, { user });
 }
