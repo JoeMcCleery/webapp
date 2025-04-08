5b506c7a4a417283eb9759edfe0bac12a8a497fd
JoeMcCleery <mccleery.joseph@gmail.com>
2025-04-08 22:54:41 +0800

    WIP auth route

---
+++ apps/api/src/routes/auth.ts
@@ -0,0 +1,3 @@
+import type { FastifyPluginCallback } from "fastify"
+
+export const auth: FastifyPluginCallback = function (app) {}
--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,6 +1,8 @@
+import { auth } from "./auth"
 import { rpc } from "./rpc"
 import type { FastifyPluginCallback } from "fastify"
 
 export const routes: FastifyPluginCallback = function (app) {
   app.register(rpc, { prefix: "/rpc" })
+  app.register(auth, { prefix: "/auth" })
 }
