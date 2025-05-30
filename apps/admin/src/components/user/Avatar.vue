adae4b4a08f08f074f8f22bcb8940b2854295450
JoeMcCleery <mccleery.joseph@gmail.com>
2025-05-31 03:19:54 +0800

    Default avatar styling

--- apps/admin/src/components/user/Avatar.vue
+++ apps/admin/src/components/user/Avatar.vue
@@ -1,14 +1,7 @@
 <template>
-  <UAvatar
-    :alt="fullName"
-    icon="i-lucide-user-round"
-    :ui="{ root: 'bg-accented' }"
-  />
+  <UAvatar :alt="fullName(auth.user)" icon="i-lucide-user-round" />
 </template>
 
 <script setup lang="ts">
 const auth = useAuthStore()
-const fullName = computed(
-  () => `${auth.user?.givenName || ""} ${auth.user?.familyName || ""}`,
-)
 </script>
